e7ea856f3ec690e01a8672e87b9da07a0ff3c20c
peterashwell <peter.w.ashwell@gmail.com>
2015-05-16 18:13:00 +1000

    Crawl and display fetched links on map

--- crawler.js
+++ crawler.js
@@ -4,23 +4,34 @@
 // Stop at boundary or all points visited
 var svInstance = new google.maps.StreetViewService();
 var allPanoramas = {};
+var map = null;
+var panorama = null;
+
+// Sample locations
+var clovelly = new google.maps.LatLng(-33.9049699,151.2510592);
+var harbourBridge = new google.maps.LatLng(-33.852001,151.211058);
 
 function initialize() {
     console.log('initializing...');
-    var harbourBridge = new google.maps.LatLng(-33.852001,151.211058);
-    var clovelly = new google.maps.LatLng(-33.9049699,151.2510592);
+    
+    drawPanorama();
+    drawMap();
+}
+
+function drawPanorama() {
     var panoramaOptions = {
         position: clovelly /*harbourBridge*/
     };
-    var panorama = new google.maps.StreetViewPanorama(
+    panorama = new google.maps.StreetViewPanorama(
         document.getElementById('pano'), panoramaOptions
     );
-    
+
     // This is when the links have loaded in the panorama
     google.maps.event.addListener(panorama, 'links_changed', function() {
         // getPanoramaById results are not like javascript pano objects
         // Convert into svInstance result so our function behaves consistently 
         svInstance.getPanoramaById(panorama.getPano(), function(result) {
+            allPanoramas[panorama.getPano()] = result;
             findNearbyPanoramas(result);
         });
         google.maps.event.addListener(panorama, 'position_changed', function() {
@@ -32,10 +43,18 @@ function initialize() {
     });
 }
 
+function drawMap() {
+    var mapOptions = {
+        zoom: 15,
+        center: clovelly
+    };
+    map = new google.maps.Map(
+        document.getElementById('map'), mapOptions
+    );
+}
+
 // Follow links until boundaries are reached, for every leaf
 // panorama hit API endpoint for adjacent (and unlinked) panoramas
-
-
 // Take list of panoramas, pop them, expand links, recurse, until
 // no more panoramas found
 function findNearbyPanoramas(pano) {
@@ -45,7 +64,13 @@ function findNearbyPanoramas(pano) {
             svInstance.getPanoramaById(link.pano, function(newData) {
                 allPanoramas[newData.location.pano] = newData;
                 console.log('found panorama, collection now:', allPanoramas);
-                if (Object.keys(allPanoramas).length < 50) {
+                debugger;
+                var marker = new google.maps.Marker({
+                    position: newData.location.latLng,
+                    map: map,
+                    title: 'panorama'
+                });
+                if (Object.keys(allPanoramas).length < 20) {
                     findNearbyPanoramas(newData);
                 }
             });
